01773bdc679eab7c989c1574c064de4b87c8ad1d
Rodrigo Jimenez Dencker <rjimenez@PO-HB751F2-LAP.pasiona.es>
2020-02-03 09:44:58 +0100

    Deleted unused middlewares

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducer';
-import logger from '../middleware/logger';
-import censure from '../middleware/censure';
 import thunk from 'redux-thunk';
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
